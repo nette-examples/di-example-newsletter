8ee9fecc17efbad4d950a186a4a80072c299d734
David <david@grudl.com>
2023-10-04 18:34:33 +0200

    added comments to the code

--- example.php
+++ example.php
@@ -3,14 +3,20 @@
 require __DIR__ . '/vendor/autoload.php';
 
 
-// create DI container
+// Set up the Dependency Injection container with auto-rebuild enabled
+// The auto-rebuild feature ensures the container is regenerated when changes are detected
 $loader = new Nette\DI\ContainerLoader(__DIR__ . '/temp', autoRebuild: true);
+
+// Load the configuration and create the DI container class
 $class = $loader->load(function($compiler) {
-    $compiler->loadConfig(__DIR__ . '/config.neon');
+	$compiler->loadConfig(__DIR__ . '/config.neon');
 });
+
+
+// Instantiate the DI container
 $container = new $class;
 
 
-// my application
-$manager = $container->getByType('NewsletterManager');
+// Fetch the 'NewsletterManager' service from the DI container and run the distribution
+$manager = $container->getByType(NewsletterManager::class);
 $manager->distribute(['john@example.com']);
